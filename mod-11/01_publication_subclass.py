8a1ff100ae9459f869434778e13e6d8f40ea12d7
tmp009 <128413103+tmp009@users.noreply.github.com>
2026-01-26 12:48:35 +0200

    mod 11 completed

---
+++ mod-11/01_publication_subclass.py
@@ -0,0 +1,20 @@
+class Publication:
+  def __init__(self, name):
+    self.name = name
+
+class Book(Publication):
+  def __init__(self, name, author, page_count):
+    super().__init__(name)
+    self.author = author
+    self.page_count = page_count
+
+  def print_information(self):
+    print(f"{self.name}, {self.author}, {self.page_count}")
+
+class Magazine(Publication):
+  def __init__(self, name, chief_editor):
+    super().__init__(name)
+    self.chief_editor = chief_editor
+
+  def print_information(self):
+    print(f"{self.name}, {self.chief_editor}")
